2c3f28adc8ba70151e1239b7f97aec28899cc114
FelixMo42 <felix@tacocat.com>
2019-08-21 11:37:17 -0700

    updated constants

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,18 +1,18 @@
 module Constants
-  TIMEZONE = 'Eastern Time (US & Canada)'
+  TIMEZONE = 'Pacific Time (US & Canada)'
   DAY_END = 1
   BUILD_MEETINGS = 2
   PRE_MEETINGS = 1
   PRE_HOURS = 25
-  DEFAULT_LOGIN = "signup@team900.org"
-  TEAM_NAME = 'Zebracorns'
-  TEAM_NUMBER = '900'
-  SCHOOL_NAME_REPLACEMENT = 'School'
+  DEFAULT_LOGIN = "signup@team5419.org"
+  TEAM_NAME = 'Natural Disasters'
+  TEAM_NUMBER = '5419'
+  SCHOOL_NAME_REPLACEMENT = 'Grade'
   GENDERS = ['Male', 'Female', 'Other']
-  FRESHMAN_HOURS = 6
-  SOPHOMORE_HOURS = 8
-  JUNIOR_HOURS = 10
-  SENIOR_HOURS = 10
-  LEADERSHIP_HOURS = 15
-  FLEX_HOURS = 15
+  FRESHMAN_HOURS = 4
+  SOPHOMORE_HOURS = 4
+  JUNIOR_HOURS = 4
+  SENIOR_HOURS = 4
+  LEADERSHIP_HOURS = 4
+  FLEX_HOURS = 0
 end
